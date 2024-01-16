6c4d62eaf0d65e9cc256200703a188b2ef6d13de
salimjonovsamandar <salimjonovsamandarbek777@gmail.com>
2024-01-16 13:27:38 +0500

    Vazifa bajarildi

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,11 @@
 const tbody = document.getElementById("tbody");
+const name = document.getElementById("name");
+const price = document.getElementById("price");
+const description = document.getElementById("description");
+const category = document.getElementById("category");
+const button = document.getElementById("button");
+const form = document.getElementById("form");
+let backData = 0;
 
 function createRov(phone, index) {
   return `
@@ -17,6 +24,44 @@ function createRov(phone, index) {
 `;
 }
 
+function validate(name, description, category, price) {
+  if (!name.value) {
+    alert("Name Kiritilishi shart");
+    name.focus();
+    return false;
+  }
+
+  if (!name.value.trim()) {
+    alert("Name Kiritilishi shart");
+    name.focus();
+    return false;
+  }
+
+  if (!price.value) {
+    alert("price Kiritilishi shart");
+    price.focus();
+    return false;
+  }
+  if (price.value <= 0) {
+    alert("price bunday son bo'lishi mumkin emas");
+    price.focus();
+    return false;
+  }
+
+  if (!description.value) {
+    alert("description Kiritilishi shart");
+    description.focus();
+    return false;
+  }
+  if (!category.value) {
+    alert("category Kiritilishi shart");
+    category.focus();
+    return false;
+  }
+
+  return true;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   fetch("https://auth-rg69.onrender.com/api/products/all", {
     method: "GET",
@@ -24,6 +69,7 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((res) => res.json())
     .then((data) => {
       if (data.length) {
+        backData = data.length;
         data.forEach((phone, index) => {
           let tr = createRov(phone, index + 1);
           tbody.innerHTML += tr;
@@ -34,3 +80,38 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log(err);
     });
 });
+
+button &&
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (validate(name, description, category, price)) {
+      let phone = {
+        name: name.value,
+        price: price.value,
+        description: description.value,
+        status: "active",
+        category_id: category.value,
+      };
+      fetch("https://auth-rg69.onrender.com/api/products", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(phone),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.id) {
+            let tr = createRov(data, backData + 1);
+            backData++;
+            tbody.innerHTML += tr;
+          }
+          form.reset();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      console.log("Validatsiyadan o'tmadi");
+    }
+  });
